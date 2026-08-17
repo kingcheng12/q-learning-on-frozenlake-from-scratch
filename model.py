398da3450fbe286a7223187b47eaf8d498dd241a
Cat2333 <gc169@duke.edu>
2026-08-17 18:28:33 -0400

    Step 4: sample_random_action

--- model.py
+++ model.py
@@ -30,8 +30,11 @@ def greedy_action(q_table, state):
     # TODO: return argmax over the action axis for this state's Q values
     return int(np.argmax(q_table[state]))
 
-# Step 4 - sample_random_action (not yet solved)
-# TODO: implement
+# Step 4 - sample_random_action
+def sample_random_action(action_space):
+    # TODO: draw a uniformly random action from the given Gymnasium action space
+
+    return int(action_space.np_random.integers(action_space.n))
 
 # Step 5 - should_explore (not yet solved)
 # TODO: implement
